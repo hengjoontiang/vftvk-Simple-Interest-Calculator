a1e9c0b1063d8fb1332fbdeb0582a0a2f39297b1
hengjt <hengjoontiang@yahoo.com>
2022-02-03 08:49:48 +0800

    updated for yellow text interest amount

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    validate();
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100.0;
+    //interest = interest.fontcolor("yellow");
+    var year = new Date().getFullYear() + parseInt(years);
+
+    var text2Display = "If you deposit "+principal+",at an interest rate of "+rate+"%.You will receive an amount of <font style='color:yellow' >"+interest+"</font> in the year "+year;
+    document.getElementById("result").innerHTML=text2Display;
 }
-        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+// Form validation code will come here.
+function validate() {
+        var principal = document.getElementById("principal").value;
+        if( principal <= 0.0  ) {
+            alert( "Enter a positive number" );
+            document.getElementById("principal").focus() ;
+            return false;
+         }
+
+         return( true );
+}
